22940fd316e24973af1e5c82e84b11f922f9c8f7
Johannes <helmes@thp.uni-koeln.de>
2016-01-13 16:34:53 +0100

    boson 45 deg corner

--- bosons/other-angles/src/free_boson_2D.py
+++ bosons/other-angles/src/free_boson_2D.py
@@ -171,13 +171,15 @@ def getRegionA((Lx,Ly),(x0,y0),bipart,angle=90):
         if bipart==1:
             regA[:x0,:]=True
         elif bipart==2:
+            for i,e in enumerate(range(x0-1,-1,-1)):
+                regA[e,0:min(Ly,y0+i+1)]=True
+
+            for i,e in enumerate(range(x0,Lx,1)):
+                regA[e,0:max(0,y0-i)]=True
             #concave corners of the pixelized line are the locations of the (x0,y0)
-            triangle_length=y0+x0-1
-            for i,e in enumerate(range(min(Lx,triangle_length))):
-                regA[e,0:min(Ly,triangle_length-i)]=True
         elif bipart==3:
-            for e,i in enumerate(range(x0,0,-1)):
-                regA[e,y0+i-1:Ly]=True
+            for i,e in enumerate(range(x0-1,-1,-1)):
+                regA[e,min(y0+i,Ly):]=True
 
     if angle==67.5:
         #y0 must be at least 1
